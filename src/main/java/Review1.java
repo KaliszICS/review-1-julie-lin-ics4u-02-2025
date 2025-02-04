ace400822da8517ffd60c05f0e7e2266095f485d
Julie Lin <julie.lin@student.tdsb.on.ca>
2025-02-04 15:16:07 +0000

    complete questions

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,22 +2,35 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int num1 = 9;
+		int num2 = 5;
+		System.out.println(num1/num2);
+		System.out.println(num1%num2); 
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		boolean trueFalse;
+		trueFalse = true;
+		System.out.println(trueFalse);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		String str1 = "Hello"; 
+		String str2 = " World";
+		System.out.print(str1); 
+		System.out.println(str2); 
 	}
 
 }
